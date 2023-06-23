33a4d11d495ffeac99305e62eeb44374bec072d4
Ashish Kar <ashacekar@gmail.com>
2023-06-23 16:48:47 +0530

    Setting up routes and components

--- cricket-app/src/index.tsx
+++ cricket-app/src/index.tsx
@@ -1,25 +1,29 @@
+import 'react-app-polyfill/ie11';
+import 'sd-ui-components/dist/styles/smartdrive.css';
+import 'core-js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorkerRegistration';
+import HttpsRedirect from 'react-https-redirect';
+import { MainRoutes } from './routes';
+import MessageProvider from './providers/MessageProvider';
+import { LocaleProvider } from './providers/LocaleProvider';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+(async () => {
+  const Root = (
+    <LocaleProvider>
+        <HttpsRedirect>
+                <MessageProvider>
+                  <MainRoutes />
+                </MessageProvider>
+        </HttpsRedirect>
+    </LocaleProvider>
+  );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+  const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+  root.render(Root);
+  serviceWorker.register();
+})();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Code-splitting using react-router
+// https://github.com/AnomalyInnovations/serverless-stack-demo-client/tree/code-splitting-in-create-react-app
