653d1bd3a932cd5ce19a6825f601d840e236c179
Ashish Kar <ashish.kar@smartdrive.net>
2023-06-28 11:43:04 +0530

    age calculation from dob corrected

--- src/__tests__/utility/ConvertMilliSecondsToAge.test.ts
+++ src/__tests__/utility/ConvertMilliSecondsToAge.test.ts
@@ -2,6 +2,6 @@ import { convertMilliSecondsToAge } from '../../utility/ConvertMilliSecondsToAge
 
 describe('check milliseconds to age conversion', () => {
   it('returns correct age', () => {
-    expect(convertMilliSecondsToAge(594691200000)).toBe(18);
+    expect(convertMilliSecondsToAge(594691200000)).toBe(34);
   });
 });
--- src/utility/ConvertMilliSecondsToAge.ts
+++ src/utility/ConvertMilliSecondsToAge.ts
@@ -1,3 +1,10 @@
 export const convertMilliSecondsToAge = (ms:number) => {
-    return Math.floor(ms / 31536000000)
+    const playerDob = new Date(ms);
+    const currTimestamp = new Date();
+    let age = currTimestamp.getFullYear() - playerDob.getFullYear();
+    let m = currTimestamp.getMonth() - playerDob.getMonth();
+    if (m < 0 || (m === 0 && currTimestamp.getDate() < playerDob.getDate())) {
+        age--;
+    }
+    return age;
   }
